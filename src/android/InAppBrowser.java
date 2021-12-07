f32d505daac9a280dac72e5e1c11e1c15c2c7e52
Gheorghe Stavila <gheorghe.stavila@getik.net>
2021-12-07 16:38:46 +0200

    comment close dialog

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1428,7 +1428,7 @@ public class InAppBrowser extends CordovaPlugin {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     handler.proceed();
-                    closeDialog();
+                    //closeDialog();
                 }
             });
             builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
